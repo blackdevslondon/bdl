a90495e58f0a4218d2dd4cd3093b2e19e2882d63
Genyus <genyus@gmail.com>
2021-03-24 23:19:51 +0000

    chore: Configures ESLint, EditorConfig and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: ["eslint:recommended", "prettier"],
+  env: {
+    es6: true,
+    node: true,
+  },
+  rules: {
+    "prettier/prettier": "error",
+  },
+  plugins: ["prettier"],
+  overrides: [
+    {
+      files: ["*.ts"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "eslint:recommended",
+        "prettier",
+      ],
+    },
+  ],
+};
