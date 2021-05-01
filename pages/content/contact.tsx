4af4fb3357efe2ac7b87b2a542f38612963f9b0e
Takuma <takumab@gmail.com>
2021-05-01 15:14:15 +0100

    Add simple contact form, Add tailwind forms plugin, Add placeholder text

--- pages/content/contact.tsx
+++ pages/content/contact.tsx
@@ -3,7 +3,20 @@ import { Navbar } from "./navbar";
 const Contact = () => (
   <div>
     <Navbar />
-    <h1>Contact Page!</h1>
+    <div className="md:container md:mx-auto">
+      <h1 className="font-bold text-5xl text-center mt-9">Contact Us</h1>
+      <article>
+        <form action="#" method="POST">
+          <label htmlFor="fname" className="block text-sm font-medium">First name:</label>
+          <input type="text" id="fname" name="fname" className="form-input" />
+          <label htmlFor="lname" className="block text-sm font-medium">Last name:</label>
+          <input type="text" id="lname" name="lname" className="form-input"/>
+          <label htmlFor="email" className="block text-sm font-medium">Last name:</label>
+          <input type="text" id="email" name="email" className="form-input"/>
+          <input type="submit" value="Submit" className="block" />
+        </form>
+      </article>
+    </div>
   </div>
 );
 export default Contact;
