434f47e2bfd71649000a412c837c54fc39894323
Takuma <takumab@gmail.com>
2021-05-15 17:48:38 +0100

    Add snapshot test for about page

---
+++ test/pages/content/about.test.tsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import About from "../../../pages/content/about";
+
+
+describe('About Page', () => {
+  it('should match snapshot', () => {
+    const { asFragment } = render(<About />, {});
+    expect(asFragment()).toMatchSnapshot()
+
+  });
+});
