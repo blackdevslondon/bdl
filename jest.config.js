ae544de8f4de0cb5666f8924edde66b990978e1d
Takuma <takumab@gmail.com>
2021-05-17 18:19:03 +0100

    Enable auto config eslint, Fix lint errors

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  roots: ['<rootDir>/test'],
+  roots: ["<rootDir>/test"],
   testMatch: [
-    '**/__tests__/**/*.+(ts|tsx|js)',
-    '**/?(*.)+(spec|test).+(ts|tsx|js)'
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
   ],
   transform: {
-    '^.+.(ts|tsx)$': 'babel-jest'
-  }
+    "^.+.(ts|tsx)$": "babel-jest",
+  },
 };
--- test/pages/content/about.test.tsx
+++ test/pages/content/about.test.tsx
@@ -1,8 +1,8 @@
 import { render } from "@testing-library/react";
 import About from "../../../pages/content/about";
 
-describe('About Page', () => {
-  it('should match snapshot', () => {
+describe("About Page", () => {
+  it("should match snapshot", () => {
     const { asFragment } = render(<About />, {});
     expect(asFragment()).toMatchSnapshot();
   });
