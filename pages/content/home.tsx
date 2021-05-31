65e7af71e01ad5fac0d01411b40fc72fd8cf801d
Takuma <takumab@gmail.com>
2021-05-31 12:01:13 +0100

    Refactor Contact Us into own component

--- pages/content/home.tsx
+++ pages/content/home.tsx
@@ -3,6 +3,7 @@ import AboutSection from "../../components/aboutSection";
 import { OurMissionSection } from "../../components/ourMissionSection";
 import { OurTeamSection } from "../../components/ourTeamSection";
 import { OurVisionSection } from "../../components/ourVisionSection";
+import { ContactUsSection } from "../../components/contactUsSection";
 
 const Home = () => (
   <div>
@@ -12,21 +13,7 @@ const Home = () => (
       <OurMissionSection />
       <OurTeamSection />
       <OurVisionSection />
-      <article>
-        <h1 className="font-bold text-5xl text-center mt-9">Contact Us</h1>
-        <p className="home-text">
-          I'm baby chambray four dollar toast man bun blog roof party gastropub
-          actually austin tumblr. Venmo brunch migas, try-hard thundercats food
-          truck viral blue bottle locavore pork belly forage kickstarter
-          humblebrag. Shaman post-ironic ennui freegan yr jean shorts iPhone
-          sartorial marfa narwhal stumptown fashion axe. Dreamcatcher bespoke
-          post-ironic vice pop-up celiac umami, vaporware jianbing. Enamel pin
-          ennui banjo tumeric edison bulb taxidermy, iPhone brooklyn prism
-          mustache ramps narwhal pour-over air plant. Small batch sriracha fixie
-          pug, subway tile asymmetrical crucifix tousled meggings synth
-          brooklyn. Try-hard lumbersexual meditation hammock polaroid tbh.
-        </p>
-      </article>
+      <ContactUsSection />
       <article>
         <p className="home-text">
           I'm baby chambray four dollar toast man bun blog roof party gastropub
