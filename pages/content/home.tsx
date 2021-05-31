0323cca486ac007eb33d13382b779719d785b039
Takuma <takumab@gmail.com>
2021-05-31 13:04:33 +0100

    Remove some placeholder text

--- pages/content/home.tsx
+++ pages/content/home.tsx
@@ -20,44 +20,6 @@ const Home = () => (
       <QuotableSection />
       <BigFooterSection />
       <SmallFooterSection />
-      <article>
-        <p className="home-text">
-          I'm baby chambray four dollar toast man bun blog roof party gastropub
-          actually austin tumblr. Venmo brunch migas, try-hard thundercats food
-          truck viral blue bottle locavore pork belly forage kickstarter
-          humblebrag. Shaman post-ironic ennui freegan yr jean shorts iPhone
-          sartorial marfa narwhal stumptown fashion axe. Dreamcatcher bespoke
-          post-ironic vice pop-up celiac umami, vaporware jianbing. Enamel pin
-          ennui banjo tumeric edison bulb taxidermy, iPhone brooklyn prism
-          mustache ramps narwhal pour-over air plant. Small batch sriracha fixie
-          pug, subway tile asymmetrical crucifix tousled meggings synth
-          brooklyn. Try-hard lumbersexual meditation hammock polaroid tbh.
-        </p>
-        <p className="home-text">
-          I'm baby chambray four dollar toast man bun blog roof party gastropub
-          actually austin tumblr. Venmo brunch migas, try-hard thundercats food
-          truck viral blue bottle locavore pork belly forage kickstarter
-          humblebrag. Shaman post-ironic ennui freegan yr jean shorts iPhone
-          sartorial marfa narwhal stumptown fashion axe. Dreamcatcher bespoke
-          post-ironic vice pop-up celiac umami, vaporware jianbing. Enamel pin
-          ennui banjo tumeric edison bulb taxidermy, iPhone brooklyn prism
-          mustache ramps narwhal pour-over air plant. Small batch sriracha fixie
-          pug, subway tile asymmetrical crucifix tousled meggings synth
-          brooklyn. Try-hard lumbersexual meditation hammock polaroid tbh.
-        </p>
-        <p className="home-text">
-          I'm baby chambray four dollar toast man bun blog roof party gastropub
-          actually austin tumblr. Venmo brunch migas, try-hard thundercats food
-          truck viral blue bottle locavore pork belly forage kickstarter
-          humblebrag. Shaman post-ironic ennui freegan yr jean shorts iPhone
-          sartorial marfa narwhal stumptown fashion axe. Dreamcatcher bespoke
-          post-ironic vice pop-up celiac umami, vaporware jianbing. Enamel pin
-          ennui banjo tumeric edison bulb taxidermy, iPhone brooklyn prism
-          mustache ramps narwhal pour-over air plant. Small batch sriracha fixie
-          pug, subway tile asymmetrical crucifix tousled meggings synth
-          brooklyn. Try-hard lumbersexual meditation hammock polaroid tbh.
-        </p>
-      </article>
     </div>
   </div>
 );
