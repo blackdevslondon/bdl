68acb8bc5159514864733c04f07864416db2595b
Takuma <takumab@gmail.com>
2021-05-31 12:56:32 +0100

    Add a few things to Big Footer section

--- components/bigFooterSection.tsx
+++ components/bigFooterSection.tsx
@@ -1,6 +1,12 @@
 export const BigFooterSection = () => (
   <div id="big-footer">
-    <h4>Black Devs UK</h4>
+    <h4 className="font-bold text-2xl mt-9">Black Devs UK</h4>
+    <p className="home-text">
+      I'm baby chambray four dollar toast man bun blog roof party gastropub
+      actually austin tumblr. Venmo brunch migas, try-hard thundercats food
+      truck viral blue bottle locavore pork belly forage kickstarter humblebrag.
+      Shaman post-ironic ennui freegan yr jean shorts iPhone
+    </p>
     <ul>
       <li>LinkedIn</li>
       <li>Instagram</li>
